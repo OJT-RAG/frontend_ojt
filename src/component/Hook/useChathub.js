8c67d43a0b6e26903c51be4f3adb64ac32f673ec
huadyng <huydang882125@gmail.com>
2026-01-21 04:19:37 +0700

    updatechat and switch to polling

--- src/component/Hook/useChathub.js
+++ src/component/Hook/useChathub.js
@@ -5,11 +5,22 @@ export default function useChatHub(userId, onMessage) {
   useEffect(() => {
     if (!userId) return;
 
-    chatHubService.connect(userId);
-    chatHubService.subscribe(onMessage);
+    let isMounted = true;
+
+    const start = async () => {
+      await chatHubService.connect(userId);
+
+      if (!isMounted) return;
+
+      chatHubService.subscribe(onMessage);
+    };
+
+    start();
 
     return () => {
+      isMounted = false;
       chatHubService.unsubscribe(onMessage);
+      chatHubService.disconnect?.(); // 👈 nếu có
     };
-  }, [userId]);
+  }, [userId, onMessage]);
 }
